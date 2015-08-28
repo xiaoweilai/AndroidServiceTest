6743765be90c83132191ffea476d1c93c98c44e5
abc <wxjlmr@126.com>
2015-08-29 00:27:00 +0800

    启动服务线程来打印信息，OK，使用的是Service222

---
+++ gen/com/example/serviceok/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.serviceok;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/serviceok/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.serviceok;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity
+{
+  private Button mButton01,mButton02;
+  
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    
+    mButton01 = (Button)findViewById(R.id.myButton1);
+    
+    /* 开始启动系统服务按钮事件 */
+    mButton01.setOnClickListener(new Button.OnClickListener()
+    {
+      @Override
+      public void onClick(View v)
+      {
+        // TODO Auto-generated method stub
+        
+        /* 建构Intent对象，指定开启对象为mService222服务 */
+        Intent i = new Intent( MainActivity.this, mService222.class );
+        
+        /* 设定新TASK的方式 */
+        i.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+        
+        /* 以startService方法启动Intent */
+        startService(i); 
+      }
+    });
+    
+    mButton02 = (Button)findViewById(R.id.myButton2);
+    
+    /* 关闭系统服务按钮事件 */
+    mButton02.setOnClickListener(new Button.OnClickListener()
+    {
+      @Override
+      public void onClick(View v)
+      {
+        // TODO Auto-generated method stub
+        
+        /* 构造Intent对象，指定欲关闭的对象为mService222服务 */
+        Intent i = new Intent( MainActivity.this, mService222.class );
+        
+        /* 以stopService方法关闭Intent */
+        stopService(i);
+      }
+    });
+  }
+}
+
+
+//
+//
+//public class MainActivity extends ActionBarActivity {
+//
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+//	}
+//
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		// Handle action bar item clicks here. The action bar will
+//		// automatically handle clicks on the Home/Up button, so long
+//		// as you specify a parent activity in AndroidManifest.xml.
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
+//}
